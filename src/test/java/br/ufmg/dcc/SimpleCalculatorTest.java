bbe4d565dd3e4486f533b5bd2f4083e6bdcf3bc2
Henrique Rocha <henrique.rocha@gmail.com>
2021-12-02 01:11:17 -0500

    Adding more unit tests Fixing the Calculator

--- src/test/java/br/ufmg/dcc/SimpleCalculatorTest.java
+++ src/test/java/br/ufmg/dcc/SimpleCalculatorTest.java
@@ -24,5 +24,38 @@ public class SimpleCalculatorTest {
     assertEquals(-2, calc.addition(4,-6));
   }
 
+  @Test
+  public void testSubtraction(){
+    assertEquals(0,calc.subtraction(1,1));
+  }
 
+  @Test
+  public void testMultiplication(){
+    assertEquals(6,calc.multiplication(2,3));
+  }
+
+  @Test
+  public void testMultiplicationIdentity(){
+    assertEquals(11,calc.multiplication(11,1));
+  }
+
+  @Test
+  public void testMultiplicationNegative(){
+    assertEquals(-10,calc.multiplication(5,-2));
+  }
+
+  @Test
+  public void testMultiplicationTwoNegatives(){
+    assertEquals(10,calc.multiplication(-5,-2));
+  }
+
+  @Test
+  public void testDivision(){
+    assertEquals(2, calc.division(8,4));
+  }
+
+  @Test
+  public void testFactorial(){
+    assertEquals(120, calc.factorial(5));
+  }
 }
