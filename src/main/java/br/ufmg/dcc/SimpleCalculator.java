26f45757bcca072d42b74349e0b135668d062ca6
Tim Xu <timx@uvic.ca>
2025-03-28 23:45:46 -0700

    Deliberately introducting a bug in addition

--- src/main/java/br/ufmg/dcc/SimpleCalculator.java
+++ src/main/java/br/ufmg/dcc/SimpleCalculator.java
@@ -3,7 +3,7 @@ package br.ufmg.dcc;
 public class SimpleCalculator {
 
   public int addition(int x, int y) {
-    return x + y;
+    return x + y + 1;
   }
 
   public int subtraction(int x, int y) {
